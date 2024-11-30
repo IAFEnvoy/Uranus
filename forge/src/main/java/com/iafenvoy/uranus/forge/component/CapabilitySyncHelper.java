0ec361b574105d5742e29e03e45430f290e6e319
IAFEnvoy <tanksheng545@126.com>
2024-11-30 17:30:42 +0800

    add living component

--- forge/src/main/java/com/iafenvoy/uranus/forge/component/CapabilitySyncHelper.java
+++ forge/src/main/java/com/iafenvoy/uranus/forge/component/CapabilitySyncHelper.java
@@ -6,6 +6,7 @@ import dev.architectury.networking.NetworkManager;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -17,6 +18,7 @@ import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -30,20 +32,29 @@ import java.util.function.Function;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CapabilitySyncHelper {
     public static final Identifier CAPABILITY_SYNC = new Identifier(Uranus.MOD_ID, "capability_sync");
-    private static final List<CapabilityHolder<? extends ICapabilityProvider, ? extends ITickableCapability>> HOLDERS = new ArrayList<>();
+    private static final List<LivingCapabilityHolder<? extends ICapabilityProvider, ? extends ITickableCapability>> LIVINGS = new ArrayList<>();
+    private static final List<PlayerCapabilityHolder<? extends ICapabilityProvider, ? extends ITickableCapability>> PLAYERS = new ArrayList<>();
 
-    public static <P extends ICapabilityProvider, S extends ITickableCapability> void register(Identifier id, Capability<S> capability, Function<PlayerEntity, P> constructor) {
-        register(id, capability, constructor, CopyPolicy.KEEP_INVENTORY);
+    public static <P extends ICapabilityProvider, S extends ITickableCapability> void registerForLiving(Identifier id, Capability<S> capability, Function<LivingEntity, P> constructor) {
+        LIVINGS.add(new LivingCapabilityHolder<>(id, capability, constructor));
     }
 
-    public static <P extends ICapabilityProvider, S extends ITickableCapability> void register(Identifier id, Capability<S> capability, Function<PlayerEntity, P> constructor, CopyPolicy copyPolicy) {
-        HOLDERS.add(new CapabilityHolder<>(id, capability, constructor, copyPolicy));
+    public static <P extends ICapabilityProvider, S extends ITickableCapability> void registerForPlayer(Identifier id, Capability<S> capability, Function<PlayerEntity, P> constructor) {
+        registerForPlayer(id, capability, constructor, CopyPolicy.KEEP_INVENTORY);
+    }
+
+    public static <P extends ICapabilityProvider, S extends ITickableCapability> void registerForPlayer(Identifier id, Capability<S> capability, Function<PlayerEntity, P> constructor, CopyPolicy copyPolicy) {
+        PLAYERS.add(new PlayerCapabilityHolder<>(id, capability, constructor, copyPolicy));
     }
 
     @SubscribeEvent
     public static void attachCapability(AttachCapabilitiesEvent<Entity> event) {
+        if (event.getObject() instanceof LivingEntity living)
+            for (LivingCapabilityHolder<?, ?> holder : LIVINGS)
+                if (!living.getCapability(holder.capability).isPresent())
+                    event.addCapability(holder.id, holder.constructor.apply(living));
         if (event.getObject() instanceof PlayerEntity player)
-            for (CapabilityHolder<?, ?> holder : HOLDERS)
+            for (PlayerCapabilityHolder<?, ?> holder : PLAYERS)
                 if (!player.getCapability(holder.capability).isPresent())
                     event.addCapability(holder.id, holder.constructor.apply(player));
     }
@@ -51,14 +62,25 @@ public class CapabilitySyncHelper {
     @SubscribeEvent
     public static void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
         if (event.getEntity() instanceof ServerPlayerEntity serverPlayer)
-            for (CapabilityHolder<?, ?> holder : HOLDERS)
+            for (PlayerCapabilityHolder<?, ?> holder : PLAYERS)
                 serverPlayer.getCapability(holder.capability).resolve().ifPresent(storage -> NetworkManager.sendToPlayer(serverPlayer, CAPABILITY_SYNC, PacketBufferUtils.create().writeIdentifier(holder.id).writeNbt(storage.serializeNBT())));
+    }
+
+    @SubscribeEvent
+    public static void onLivingTick(LivingEvent.LivingTickEvent event) {
+        LivingEntity living = event.getEntity();
+        for (LivingCapabilityHolder<?, ? extends ITickableCapability> holder : LIVINGS) {
+            Optional<? extends ITickableCapability> optional = living.getCapability(holder.capability).resolve();
+            if (optional.isEmpty()) continue;
+            ITickableCapability capability = optional.get();
+            capability.tick();
+        }
     }
 
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
         PlayerEntity player = event.player;
-        for (CapabilityHolder<?, ? extends ITickableCapability> holder : HOLDERS) {
+        for (PlayerCapabilityHolder<?, ? extends ITickableCapability> holder : PLAYERS) {
             Optional<? extends ITickableCapability> optional = player.getCapability(holder.capability).resolve();
             if (optional.isEmpty()) continue;
             ITickableCapability capability = optional.get();
@@ -71,7 +93,7 @@ public class CapabilitySyncHelper {
     @SubscribeEvent
     public static void onPlayerClone(PlayerEvent.Clone event) {
         PlayerEntity origin = event.getOriginal(), player = event.getEntity();
-        for (CapabilityHolder<?, ? extends ITickableCapability> holder : HOLDERS) {
+        for (PlayerCapabilityHolder<?, ? extends ITickableCapability> holder : PLAYERS) {
             if (event.isWasDeath() && !holder.copyPolicy.shouldCopyOnDeath(player.getEntityWorld())) continue;
             Optional<? extends ITickableCapability> originalCapability = origin.getCapability(holder.capability).resolve();
             Optional<? extends ITickableCapability> newCapability = player.getCapability(holder.capability).resolve();
@@ -87,7 +109,7 @@ public class CapabilitySyncHelper {
             NetworkManager.registerReceiver(NetworkManager.Side.S2C, CAPABILITY_SYNC, (buf, context) -> {
                 Identifier id = buf.readIdentifier();
                 NbtCompound compound = buf.readNbt();
-                HOLDERS.stream().filter(x -> x.id.equals(id)).findFirst().ifPresent(holder -> context.queue(() -> {
+                PLAYERS.stream().filter(x -> x.id.equals(id)).findFirst().ifPresent(holder -> context.queue(() -> {
                     ClientPlayerEntity player = MinecraftClient.getInstance().player;
                     if (player != null)
                         player.getCapability(holder.capability).resolve().ifPresent(x -> x.deserializeNBT(compound));
@@ -96,13 +118,25 @@ public class CapabilitySyncHelper {
         }
     }
 
-    public static class CapabilityHolder<P extends ICapabilityProvider, S extends ITickableCapability> {
+    public static class LivingCapabilityHolder<P extends ICapabilityProvider, S extends ITickableCapability> {
+        private final Identifier id;
+        private final Capability<S> capability;
+        private final Function<LivingEntity, P> constructor;
+
+        public LivingCapabilityHolder(Identifier id, Capability<S> capability, Function<LivingEntity, P> constructor) {
+            this.id = id;
+            this.capability = capability;
+            this.constructor = constructor;
+        }
+    }
+
+    public static class PlayerCapabilityHolder<P extends ICapabilityProvider, S extends ITickableCapability> {
         private final Identifier id;
         private final Capability<S> capability;
         private final Function<PlayerEntity, P> constructor;
         private final CopyPolicy copyPolicy;
 
-        public CapabilityHolder(Identifier id, Capability<S> capability, Function<PlayerEntity, P> constructor, CopyPolicy copyPolicy) {
+        public PlayerCapabilityHolder(Identifier id, Capability<S> capability, Function<PlayerEntity, P> constructor, CopyPolicy copyPolicy) {
             this.id = id;
             this.capability = capability;
             this.constructor = constructor;
