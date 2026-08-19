06f19778e02bbe35925255033f403ac6aa0c2ca6
IAFEnvoy <tanksheng545@126.com>
2026-08-19 15:16:40 +0800

    small fixes

--- src/main/java/com/iafenvoy/uranus/Uranus.java
+++ src/main/java/com/iafenvoy/uranus/Uranus.java
@@ -1,12 +1,10 @@
 package com.iafenvoy.uranus;
 
 import com.mojang.logging.LogUtils;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
 @Mod(Uranus.MOD_ID)
-@EventBusSubscriber
 public final class Uranus {
     public static final String MOD_ID = "uranus";
     public static final Logger LOGGER = LogUtils.getLogger();
